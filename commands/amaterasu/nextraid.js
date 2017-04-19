ca7486e52b15680f13966f56d73b69c6bef7d073
Trenton Kress <trenton.kress@gmail.com>
2017-04-19 11:42:06 -0700

    Add more to raid sheet parse

--- commands/amaterasu/nextraid.js
+++ commands/amaterasu/nextraid.js
@@ -1,10 +1,12 @@
 const GSS = require('google-spreadsheet');
-let raidSheet = GSS('1pSs5-carUxCgMx4uh1lfIT0NTKx9ZEVvgt7Cama2krc');
+let raidSheet = new GSS('1pSs5-carUxCgMx4uh1lfIT0NTKx9ZEVvgt7Cama2krc');
 
 module.exports = function(client, msg, args) {
   msg.channel.send('Loading raid spreadsheet');
   raidSheet.getInfo(function(err, info) {
     if (err) console.log(err);
     msg.channel.send('Loaded sheet: ' + info.title + ' by ' + info.author.email);
+    let sheet = info.worksheets[0];
+    msg.channel.send('Schedule sheet: ' + sheet.title + ' ' + sheet.rowCount + 'x' + sheet.colCount);
   });
 };
