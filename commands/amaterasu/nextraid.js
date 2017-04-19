3de391bf3e22bf9da4ce34662b511088ee89cb4b
Trenton Kress <trenton.kress@gmail.com>
2017-04-19 11:11:04 -0700

    Add raid spreadsheet google credentials

--- commands/amaterasu/nextraid.js
+++ commands/amaterasu/nextraid.js
@@ -1,3 +1,10 @@
+const GSS = require('google-spreadsheet');
+let raidSheet = GSS('1pSs5-carUxCgMx4uh1lfIT0NTKx9ZEVvgt7Cama2krc');
+
 module.exports = function(client, msg, args) {
-  msg.channel.send();
+  msg.channel.send('Loading raid spreadsheet');
+  raidSheet.getInfo(function(err, info) {
+    if (err) console.log(err);
+    msg.channel.send('Loaded sheet: ' + info.title + ' by ' + info.author.email);
+  });
 };
