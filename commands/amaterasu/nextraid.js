766ea1b879c6130093341c129186b5513082d441
Trenton Kress <trenton.kress@gmail.com>
2017-04-25 14:14:42 -0700

    Parse characters from dpsinject and find user's characters in each raid

--- commands/amaterasu/nextraid.js
+++ commands/amaterasu/nextraid.js
@@ -1,12 +1,44 @@
-const GSS = require('google-spreadsheet');
-let raidSheet = new GSS('1pSs5-carUxCgMx4uh1lfIT0NTKx9ZEVvgt7Cama2krc');
+const https = require('https');
+const config = require('../../config');
 
 module.exports = function(client, msg, args) {
-  msg.channel.send('Loading raid spreadsheet');
-  raidSheet.getInfo(function(err, info) {
-    if (err) console.log(err);
-    msg.channel.send('Loaded sheet: ' + info.title + ' by ' + info.author.email);
-    let sheet = info.worksheets[0];
-    msg.channel.send('Schedule sheet: ' + sheet.title + ' ' + sheet.rowCount + 'x' + sheet.colCount);
+  https.get('https://dpsinject.us/api/v1/discord/user/id/' + msg.author.id + '/characters', function(res) {
+    res.on('data', function(data) {
+      let mtChars = JSON.parse(data).characters.filter(c => c.charServer == 'Mount Tyrannas').map(c => c.charName);
+      https.get({
+        hostname:'dpsinject.us',
+        port: 443,
+        path: '/api/v1/raid/schedule',
+        method: 'GET',
+        headers: { Cookie: 'connect.sid=' + config.DPSINJECT_SID }
+      }, function(res) {
+        res.on('data', function(data) {
+          let info = JSON.parse(data);
+          let isIn = [];
+          let curr = info.raids[0];
+          while(curr) {
+            checkRaid(curr, mtChars, curr.dps, isIn);
+            checkRaid(curr, mtChars, curr.tank, isIn);
+            checkRaid(curr, mtChars, curr.priest, isIn);
+            checkRaid(curr, mtChars, curr.mystic, isIn);
+            curr = info.raids[curr.num++];
+          };
+          let output = '\`\`\`';
+          isIn.forEach(inR => output += info.raids[inR.idx].dayOfWeek + ' ' + info.raids[inR.idx].date + ' at ' + info.raids[inR.idx].time + ' on ' + inR.char + '\n');
+          output += '\`\`\`';
+          msg.channel.send(output);
+        });
+      });
+    });
   });
 };
+
+function checkRaid(raid, myChars, raidChars, isIn) {
+  raidChars.forEach(rChar => {
+    myChars.forEach(mChar => {
+      if (mChar == rChar.name) {
+        isIn.push({char: mChar, idx: raid.num});
+      }
+    });
+  });
+}
