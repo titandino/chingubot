89c493036f542a57d0e05c8334e15fff0a70fbb2
FunkyPuffin <bleunoser2011@gmail.com>
2017-05-04 13:21:45 -0400

    Delete CBModule.js

--- commands/amaterasu/CBModule.js
+++
@@ -1,6 +0,0 @@
-var cleverbot = require("cleverbot.io"),
-bot = new cleverbot("rvCFWUb2LdAseoPa", "zMYnctNen0jIQjEtG9K6U6YODwdeGmve");
-bot.setNick("Amaterasu")
-bot.ask("Are you lonely", function (err, response) {
-  console.log(response);
-}
