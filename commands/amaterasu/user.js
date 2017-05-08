a1613a561625b3c1b89ac138f4c2c1752dde532d
FunkyPuffin <bleunoser2011@gmail.com>
2017-05-07 22:27:17 -0400

    Update user.js

--- commands/amaterasu/user.js
+++ commands/amaterasu/user.js
@@ -1,7 +1,8 @@
+const LOGIN_TOKEN = process.env.LOGIN_TOKEN || require('./config').LOGIN_TOKEN;
 var cleverbot = require("./cleverbot.js");
 var readline = require('readline');
 var rl = readline.createInterface(process.stdin, process.stdout);
-bot = new cleverbot('');
+//bot = new cleverbot('');  This is apparently wrong
 
 bot.setNick("Amaterasu"); // This is optional
 bot.create(function (err, response) {
